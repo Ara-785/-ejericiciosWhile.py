04612934869d29033d15cad7a8be5ad8e90e0ebf
Ara-785 <valeriedarjoy@gmail.com>
2024-10-17 19:51:22 -0500

    Create Punto1.5.py

---
+++ Punto1.5.py
@@ -0,0 +1,10 @@
+# Solicitar al usuario que ingrese la palabra "Jengibre"  
+palabra = input("Introduce la palabra 'Jengibre': ")  
+
+# Verificar la palabra ingresada y emitir el mensaje correspondiente  
+if palabra == "Jengibre":  
+    print("Sí, ¡El Jengibre es la mejor planta de todos los tiempos!")  
+elif palabra == "jengibre":  
+    print("No, ¡quiero un gran Jengibre!")  
+else:  
+    print(f"! Jengibre¡ No, la palabra ingresada fue: {palabra}")
